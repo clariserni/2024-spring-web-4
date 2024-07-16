decbe7358de556d3e4ad4a782f42163898855f76
clariserni <clarissaserni.96@gmail.com>
2024-07-16 11:13:54 +0200

    first commit

---
+++ src/main/java/com/fattoria/FattoriaApplication.java
@@ -0,0 +1,13 @@
+package com.fattoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FattoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FattoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fattoria/FattoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fattoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FattoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
